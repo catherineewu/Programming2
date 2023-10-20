6474f3c0eddd9444ade215543660a6bdf9dd756c
Catherine Wu <139652546+catherineewu@users.noreply.github.com>
2023-10-19 21:09:21 -0400

    upload

--- Lab 4 - Binary File Input:Output/main.cpp
+++ Lab 4 - Binary File Input:Output/main.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-class Weapon {
+class Weapon { // class which is also attribute of Spaceship
 public:
     string name;
     int power;
@@ -16,14 +17,14 @@ public:
     }
 };
 
-class Spaceship {
+class Spaceship { // each spaceship read into class object
 public:
     string name;
     string ship_class;
     short length;
     int shield;
     float warp;
-    int total_power;
+    int total_power = 0;
     vector<Weapon> weapons;
     Spaceship(string name, string ship_class, short length, int shield, float warp, vector<Weapon> weapons) {
         this->name = name;
@@ -39,151 +40,207 @@ public:
     }
     void PrintShip() const;
 };
-
-void OpenShipFile(string file_name, vector<Spaceship>& ship_store) {
-    file_name = "C:\\" + file_name;
-    ifstream ship;
-    ship.open(file_name, ios_base::binary);  // open binary file storing ships
-
-    int ships;  // count of # of ships in file
-    int name_length;  // length of name, including null terminator
-    string name;  // string data for name, including null terminator
-    int class_length;  // length of ship's class name, including null terminator
-    string ship_class;  // string data for class, inc. null terminator
-    short length;  // ship's length (in m)
-    int shield;  // shield capacity (in teraJoules)
-    int warp;  // max warp speed of ship
-    int weapons;  // count of # of weapons equipped on ship
-    vector<Weapon> weapon_store;  // vector of Vector objects
-
-    if (ship.is_open()) {
-        ship.read((char*) &ships, 4);  // SHIPS
-        for (int i=0;i<ships;i++) {  // Loops through for each ship
-
-            /** Reading Name*/
-            ship.read((char*) &name_length, 4);  // NAME_LENGTH
-            char* name_array = new char[name_length];  // Array to store characters
-            for (int i=0;i<name_length;i++) {  // Read into array
-                ship.read((char*) &name_array[i], 4);
-            }
-            for (int i=0;i<name_length;i++) {  // Convert array to string
-                name += name_array[i];
-            }
-            delete[] name_array;
-            name_array = nullptr;
-
-            /** Reading Class */
-            ship.read((char*) &class_length, 4);  // CLASS_LENGTH
-            char* class_array = new char[class_length];  // Array to store characters
-            for (int i=0;i<class_length;i++) {  // Read into array
-                ship.read((char*) &class_array[i], 4);
-            }
-            for (int i=0;i<class_length;i++) {  // Convert array to string
-                ship_class += class_array[i];
-            }
-            delete[] class_array;
-            class_array = nullptr;
-
-            /** Reading Values */
-            ship.read((char*) &length, 2);
-            ship.read((char*) &shield, 4);
-            ship.read((char*) &warp, 4);
-            ship.read((char*) &weapons, 4);
-
-            /** Reading Weapon Files */
-            if (weapons != 0) {
-                int weapon_name_length;
-                string weapon_name;
-                int power;
-                float power_consumption;
-                for (int i=0;i<weapons;i++) {
-                    /** Reading weapon name */
-                    ship.read((char*) &weapon_name_length, 4);
-                    char* wn_array = new char[weapon_name_length];  // Array to store characters
-                    for (int i=0;i<weapon_name_length;i++) {  // Read into array
-                        ship.read((char*) &wn_array[i], 4);
-                    }
-                    for (int i=0;i<weapon_name_length;i++) {  // Convert array to string
-                        weapon_name += wn_array[i];
-                    }
-                    delete[] wn_array;
-                    wn_array = nullptr;
-
-                    /** Reading Values */
-                    ship.read((char*) &power, 4);
-                    ship.read((char*) &power_consumption, 4);
-
-                    Weapon current_weapon(weapon_name, power, power_consumption);
-                    weapon_store.push_back(current_weapon);
-                }
-            }
-            Spaceship current_ship(name, ship_class, length, shield, warp, weapon_store);
-            ship_store.push_back(current_ship);
-        }
-    }
-}
-
 void Spaceship::PrintShip () const {
     cout << "Name: " << name << endl;
     cout << "Class: " << ship_class << endl;
     cout << "Length: " << length << endl;
     cout << "Shield capacity: " << shield << endl;
-    cout << "Maximum warp: " << warp << endl;
-    cout << "Armaments:" << endl;
+    cout << "Maximum Warp: " << setprecision(2) << warp << endl;
+    cout << "Armaments: " << endl;
     if (weapons.empty()) {
-        cout << "Unarmed" << endl;
+        cout << "Unarmed";
     } else {
         for (int f = 0; f < weapons.size(); f++) {
             cout << weapons[f].name;
             cout << ", " << weapons[f].power;
-            cout << ", " << weapons[f].power_consumption;
+            cout << ", ";
+            if (weapons[f].power_consumption > 10) {
+                cout << setprecision(3) << weapons[f].power_consumption;
+            } else {
+                cout << setprecision(2) << weapons[f].power_consumption;
+            }
             cout << endl;
         }
-        cout << "Total firepower: " << total_power << endl;
+        cout << "Total firepower: " << total_power;
     }
-    cout << endl;
 }
-void PrintShips (const vector<Spaceship>& ships) {
-    for (int i=0;i<ships.size();i++) {
-        ships[i].PrintShip();
+void PrintShips(vector<Spaceship>& ship_vector) {
+    for (int i=0;i<ship_vector.size();i++) {
+        ship_vector[i].PrintShip();
+        if (i != (ship_vector.size()-1)) {
+            cout << endl << endl;
+        }
     }
-};
+}
+void PrintUnarmedShips(vector<Spaceship>& ship_vector) {
+    vector<Spaceship> unarmed_ships;
+    for (int i = 0; i < ship_vector.size(); i++) {
+        if (ship_vector[i].total_power == 0) {
+            unarmed_ships.push_back(ship_vector[i]);
+        }
+    }
+    for (int j = 0; j < unarmed_ships.size(); j++) {
+        unarmed_ships[j].PrintShip();
+        if (j != (unarmed_ships.size() - 1)) {
+            cout << endl << endl;
+        }
+    }
+}
 
-int main_lab_4()
-{
-	cout << "Which file(s) to open?\n";
-	cout << "1. friendlyships.shp" << endl;
-	cout << "2. enemyships.shp" << endl;
-	cout << "3. Both files" << endl;
-	int option;
-	cin >> option;
+void OpenShipFile(string file_name, vector<Spaceship>& ship_vector) {
+    ifstream shipFile(file_name, ios_base::binary | ios_base::in);
+    if (shipFile.is_open()) {
+        int ship_count;
+        shipFile.read(reinterpret_cast<char*>(&ship_count), 4);
+        for (int i=0;i<ship_count;i++) {
+            int name_length;
+            string name;
+            int class_length;
+            string ship_class;
+            short length;
+            int shield;
+            float warp;
+            int weapon_count;
 
-    vector<Spaceship> ships;
+            shipFile.read(reinterpret_cast<char*>(&name_length), 4);
+
+            char* buffer = new char[name_length];
+            shipFile.read(buffer, name_length);
+            name = buffer;
+            // for (int j=0;j<name_length;j++) {
+            //     char temp;
+            //     shipFile.read(reinterpret_cast<char *>(&temp), 1);
+            //     name += temp;
+            // }
+
+            shipFile.read(reinterpret_cast<char*>(&class_length), 4);
+            char* buffer2 = new char[class_length];
+            shipFile.read(buffer2, class_length);
+            ship_class = buffer2;
+            // for (int j=0;j<class_length;j++) {
+            //     char temp;
+            //     shipFile.read(reinterpret_cast<char *>(&temp), 1);
+            //     ship_class += temp;
+            // }
+
+            shipFile.read(reinterpret_cast<char*>(&length), 2);
+            shipFile.read(reinterpret_cast<char*>(&shield), 4);
+            shipFile.read(reinterpret_cast<char*>(&warp), 4);
+            shipFile.read(reinterpret_cast<char*>(&weapon_count), 4);
+
+            vector<Weapon> weapon_vector;
+            if (weapon_count != 0) {
+                for (int k = 0; k < weapon_count; k++) {
+                    int weapon_name_length;
+                    string weapon_name;
+                    int power;
+                    float power_consumption;
+
+                    shipFile.read(reinterpret_cast<char *>(&weapon_name_length), 4);
+                    //cout << "Length of weapon name: " << weapon_name_length << endl;
+                    char* buff = new char[weapon_name_length];
+                    shipFile.read(buff, weapon_name_length);
+                    weapon_name = buff;
+                    // for (int m = 0; m < weapon_name_length; m++) {
+                    //     char temp;
+                    //     shipFile.read(reinterpret_cast<char *>(&temp), 1);
+                    //     weapon_name += temp;
+                    // }
+                    //cout << "Weapon Name: " << weapon_name << endl;
+
+                    shipFile.read(reinterpret_cast<char*>(&power), 4);
+                    shipFile.read(reinterpret_cast<char*>(&power_consumption), 4);
+                    //cout << "Power: " << power << ", Consumption: " << power_consumption << endl;
+
+                    Weapon current_weapon(weapon_name, power, power_consumption);
+                    weapon_vector.push_back(current_weapon);
+                }
+            }
+            Spaceship current_ship(name, ship_class, length, shield, warp, weapon_vector);
+            ship_vector.push_back(current_ship);
+            // current_ship.PrintShip();
+        }
+    }
+}
+
+Spaceship* FindStrongestWeapon(vector<Spaceship>& ship_vector) {
+    int strongest_weapon_power = 0;
+    Spaceship* strongest_weapon_ship;
+    for (int i=0;i<ship_vector.size();i++) {
+        for (int j=0;j<ship_vector[i].weapons.size();j++) {
+            if (ship_vector[i].weapons[j].power > strongest_weapon_power) {
+                strongest_weapon_power = ship_vector[i].weapons[j].power;
+                strongest_weapon_ship = &ship_vector[i];
+            }
+        }
+    }
+    return strongest_weapon_ship;
+}
+Spaceship* FindMostPowerful(vector<Spaceship>& ship_vector) {
+    int most_power = 0;
+    Spaceship* most_powerful_ship;
+    for (int i=0;i<ship_vector.size();i++) {
+        if (ship_vector[i].total_power > most_power) {
+            most_power = ship_vector[i].total_power;
+            most_powerful_ship = &ship_vector[i];
+        }
+    }
+    return most_powerful_ship;
+}
+Spaceship* FindWeakest(vector<Spaceship>& ship_vector) {
+    int least_power = ship_vector[0].total_power;
+    Spaceship* least_powerful_ship;
+    for (int i=0;i<ship_vector.size();i++) {
+        if (ship_vector[i].total_power != 0) {
+            if (ship_vector[i].total_power < least_power) {
+                least_power = ship_vector[i].total_power;
+                least_powerful_ship = &ship_vector[i];
+            }
+        }
+    }
+    return least_powerful_ship;
+}
+
+int main() {
+    cout << "Which file(s) to open?\n";
+    cout << "1. friendlyships.shp" << endl;
+    cout << "2. enemyships.shp" << endl;
+    cout << "3. Both files" << endl;
+    int option;
+    cin >> option;
+
+    vector<Spaceship> ship_vector;
 
     if (option == 1) {
-        OpenShipFile("friendlyships.shp", ships);
+        OpenShipFile("friendlyships.shp", ship_vector);
     } else if (option == 2) {
-        OpenShipFile("enemyships.shp", ships);
+        OpenShipFile("enemyships.shp", ship_vector);
     } else if (option == 3) {
-        OpenShipFile("friendlyships.shp", ships);
-        OpenShipFile("enemyships.shp", ships);
+        OpenShipFile("friendlyships.shp", ship_vector);
+        OpenShipFile("enemyships.shp", ship_vector);
     }
 
-	cout << "1. Print all ships" << endl;
-	cout << "2. Starship with the strongest weapon" << endl;
-	cout << "3. Strongest starship overall" << endl;
-	cout << "4. Weakest ship (ignoring unarmed)" << endl;
-	cout << "5. Unarmed ships" << endl;
-	
-	cin >> option;
+    cout << "1. Print all ships" << endl;
+    cout << "2. Starship with the strongest weapon" << endl;
+    cout << "3. Strongest starship overall" << endl;
+    cout << "4. Weakest ship (ignoring unarmed)" << endl;
+    cout << "5. Unarmed ships" << endl;
 
+    cin >> option;
+    // menu options
     if (option == 1) {
-        PrintShips(ships);
+        PrintShips(ship_vector);
     } else if (option == 2) {
-
+        Spaceship* strongest_weapon_ship = FindStrongestWeapon(ship_vector);
+        (*strongest_weapon_ship).PrintShip();
+    } else if (option == 3) {
+        Spaceship* most_powerful_ship = FindMostPowerful(ship_vector);
+        (*most_powerful_ship).PrintShip();
+    } else if (option == 4) {
+        Spaceship* weakest_ship = FindWeakest(ship_vector);
+        (*weakest_ship).PrintShip();
+    } else if (option == 5) {
+        PrintUnarmedShips(ship_vector);
     }
-	
-	/* Work your magic here */
-	
-   return 0;
+    return 0;
 }
